a8e4bc6db82c80c804f9d942a2a728e87f6a25ba
jhwang2372 <jhwang2372@gmail.com>
2018-01-23 11:13:15 +0800

    1st working edition on Windows
    Updated as the 1st working edition on Windows except the data/models

--- tools_original/demo.py
+++ tools_original/demo.py
@@ -141,9 +141,8 @@ if __name__ == '__main__':
     for i in xrange(2):
         _, _= im_detect(net, im)
 
-    # im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-    #            '001763.jpg', '004545.jpg']
-    im_names = ['2.jpeg', '3.jpeg', '6.jpeg', '9.jpeg']
+    im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
+                '001763.jpg', '004545.jpg']
     for im_name in im_names:
         print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
         print 'Demo for data/demo/{}'.format(im_name)
